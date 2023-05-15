4645a7dbc902207fc6ea6194b13c3ff4b0549868
ViniciusSPDP <vinicius_saraiva2012@hotmail.com>
2023-05-15 00:27:48 -0300

    Pratica Verificador de Idade

--- Pratica/3/js.js
+++ Pratica/3/js.js
@@ -1,23 +1,65 @@
-function carregar() {
-    var msg = document.querySelector(".msg")
-    var img = document.querySelector(".img")
+function verificar(){
+    var date = new Date()
+    var ano = date.getFullYear()
 
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são exatamente ${hora} horas`
+    var hano = document.querySelector('.hano')
+    var res = document.querySelector(".res")
 
-    if(hora >= 6 && hora<=12){
-        img.src = "IMG/1.jpg"
-        document.body.style.background = "#F8A26C"
-    }else if(hora>12 && hora<=18){
-        img.src = "IMG/2.jpg"
-        document.body.style.background = "#FF6009"
+    if (hano.value.length == 0 || Number(hano.value) > ano){
+        alert('Verifique os dados !')
     }else{
-        img.src = "IMG/3.jpg"
-        document.body.style.background = "#5C4567"
+        var sex = document.getElementsByName('rad')
+        var idade = ano - Number(hano.value)
+        var genero = ''
+
+        //Criando IMG
+        var img = document.createElement("img")
+        img.setAttribute('id', 'foto')
+
+
+        if(sex[0].checked){
+            genero = 'Masculino'
+
+            if(idade >=0 && idade<=10){
+                img.setAttribute('src', 'homem/1.jpg')
+
+            }else if(idade<=21){
+                img.setAttribute('src', 'homem/2.jpg')
+
+            }else if(idade<50){
+                img.setAttribute('src', 'homem/3.jpg')
+
+            }else{
+                img.setAttribute('src', 'homem/4.jpg')
+
+            }
+
+
+        }else if(sex[1].checked){
+            genero = 'Feminino'
+
+            if(idade >=0 && idade<=10){
+                img.setAttribute('src', 'mulher/1.png')
+
+
+            }else if(idade<=21){
+                img.setAttribute('src', 'mulher/2.png')
+
+
+            }else if(idade<50){
+                img.setAttribute('src', 'mulher/3.png')
+
+
+            }else{
+                img.setAttribute('src', 'mulher/4.png')
+
+
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Seu sexo é ${genero} com ${idade} anos`
+        res.appendChild(img)
     }
 
+
 }
-
-
-
